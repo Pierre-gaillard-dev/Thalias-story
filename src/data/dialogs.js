660e7494937b13e34437233653b26e74d01832f2
Pierre <pierre.gaillard56@orange.fr>
2025-01-30 14:23:40 +0100

    scenes 2 & 3

--- src/data/dialogs.js
+++ src/data/dialogs.js
@@ -272,7 +272,7 @@ export const dialogs = [
 	},
 	{
 		id: "R1A1S1T19",
-		question: "Oui pourquoi pas, je pourrais venter vos mérites.",
+		question: "Oui pourquoi pas, je pourrais vanter vos mérites.",
 		player_name: "Aelis",
 		player_img: "default",
 		pnj_id: "GardePerso",
@@ -282,6 +282,659 @@ export const dialogs = [
 		answered: null,
 		next_card: function () {
 			return "R1A1S2T1"
+		},
+	},
+	{
+		id: "R1A1S2T1",
+		question: "Pourquoi nous regardent-ils comme ça... ?",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "GardePerso",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-couloir",
+		answered: null,
+		next_card: function () {
+			return "R1A1S2T2"
+		},
+	},
+	{
+		id: "R1A1S2T2",
+		question:
+			"Il a dû se passer quelque chose dont nous ne sommes pas au courant...",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "GardePerso",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-couloir",
+		answered: null,
+		next_card: function () {
+			return "R1A1S2T3"
+		},
+	},
+	{
+		id: "R1A1S2T3",
+		question:
+			"Nous n’avons pas été coupé du monde pour autant, cela doit être récent.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "GardePerso",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-couloir",
+		answered: null,
+		next_card: function () {
+			return "R1A1S2T4"
+		},
+	},
+	{
+		id: "R1A1S2T4",
+		question:
+			"Je pense que sa Majesté votre Père vous en parlera directement.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "GardePerso",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-couloir",
+		answered: null,
+		next_card: function () {
+			return "R1A1S2T5"
+		},
+	},
+	{
+		id: "R1A1S2T5",
+		question: "J’espère que ce n’est pas très grave...",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "GardePerso",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-couloir",
+		answered: null,
+		next_card: function () {
+			return "R1A1S2T6"
+		},
+	},
+	{
+		id: "R1A1S2T6",
+		question:
+			"Bon, je vais entrer, je vais vous laisser vaquer à vos occupations.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "GardePerso",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-couloir",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T1"
+		},
+	},
+	{
+		id: "R1A1S3T1",
+		question:
+			"Son Altesse Royale, Mademoiselle Aelis Silundral, héritière du trône d’Auralys, fille de sa Majesté Tharion Silundral, roi et gardien de la paix d’Auralys vient d’arriver.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Garde_elfique",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T2"
+		},
+	},
+	{
+		id: "R1A1S3T2",
+		question: "Bonjour Père.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T3"
+		},
+	},
+	{
+		id: "R1A1S3T3",
+		question:
+			"Ma chère Aelis, bon retour parmi nous ! Comment s’est passé ton voyage à Pavrinthia ? La maladie a été éradiquée ?",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T4"
+		},
+	},
+	{
+		id: "R1A1S3T4",
+		question:
+			"Tout s’est bien passé merci, ça a duré plus de temps que prévu mais oui, la maladie est bel et bien éliminée.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T5"
+		},
+	},
+	{
+		id: "R1A1S3T5",
+		question:
+			"Où est Thalia ? Elle n’était pas là quand je suis descendue.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T6"
+		},
+	},
+	{
+		id: "R1A1S3T6",
+		question: "J’allais y venir ma fille, j’allais y venir.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T7"
+		},
+	},
+	{
+		id: "R1A1S3T7",
+		question: "Votre sœur nous a quitté, cela fa-",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T8"
+		},
+	},
+	{
+		id: "R1A1S3T8",
+		question: "Pardon ? Je crois que j’ai du mal vous comprendre Père.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T9"
+		},
+	},
+	{
+		id: "R1A1S3T9",
+		question: "Thalia nous a quitté. Elle n’est plus de ce monde.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T10"
+		},
+	},
+	{
+		id: "R1A1S3T10",
+		question: "Vous mentez. Thalia...",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T11"
+		},
+	},
+	{
+		id: "R1A1S3T11",
+		question: "Ma sœur ne peut pas mourir maintenant !",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T12"
+		},
+	},
+	{
+		id: "R1A1S3T12",
+		question:
+			"Aelis, je sais que c’est dur à accepter, mais c’est la vérité.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T13"
+		},
+	},
+	{
+		id: "R1A1S3T13",
+		question:
+			"Cela fera bientôt 3 ans que ta sœur a été tuée, mais nous avons fait notre deuil.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T14"
+		},
+	},
+	{
+		id: "R1A1S3T14",
+		question: "3 ans ?!",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T15"
+		},
+	},
+	{
+		id: "R1A1S3T15",
+		question:
+			"Et vous n’avez pas pensé à m’envoyer un courrier pour me prévenir Père ?!",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T16"
+		},
+	},
+	{
+		id: "R1A1S3T16",
+		question:
+			"Avec ta mère, nous ne voulions pas que cela change quoi que ce soit pour le travail que tu avais à accomplir.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T17"
+		},
+	},
+	{
+		id: "R1A1S3T17",
+		question: "Mais c’est ma sœur ! Celle qui était sensée vous succéder !",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T18"
+		},
+	},
+	{
+		id: "R1A1S3T18",
+		question: "Elle serait morte comment ?",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T19"
+		},
+	},
+	{
+		id: "R1A1S3T19",
+		question:
+			"Nous avons de fortes raisons de penser que le fils ainé de Childéric, Evender, l’a assassinée. Thalia devait l’épouser pour assurer la paix de nos royaumes.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T20"
+		},
+	},
+	{
+		id: "R1A1S3T20",
+		question: "Donc vous avez voulu la marier à son assassin ?",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T21"
+		},
+	},
+	{
+		id: "R1A1S3T21",
+		question:
+			"Je te conseille de descendre d’un ton Aelis. Nous n’aurions pas pu le prévoir.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T22"
+		},
+	},
+	{
+		id: "R1A1S3T22",
+		question: "...",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T23"
+		},
+	},
+	{
+		id: "R1A1S3T23",
+		question: "Qu’est-il advenu d’Evender ?",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T24"
+		},
+	},
+	{
+		id: "R1A1S3T24",
+		question:
+			"Childéric était censé l’envoyer, mais nous ne nous faisons plus d’illusion. Nous irons le chercher de force.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T25"
+		},
+	},
+	{
+		id: "R1A1S3T25",
+		question:
+			"Ils prétendent nous l’avoir envoyé mais nous ne l’avons pas reçu.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T26"
+		},
+	},
+	{
+		id: "R1A1S3T26",
+		question:
+			"Ces pauvres Malartiens doivent surement faire tout ce qu’ils peuvent pour protéger l’héritier du trône.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T27"
+		},
+	},
+	{
+		id: "R1A1S3T27",
+		question: "Pas surprenant venant de la part de ces barbares !",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T28"
+		},
+	},
+	{
+		id: "R1A1S3T28",
+		question:
+			"Donc vous avez laissé l’assassin de ma sœur en liberté alors que vous saviez qui l’a tuée ?",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T29"
+		},
+	},
+	{
+		id: "R1A1S3T29",
+		question:
+			"Ça ne saura tarder ma fille, il sera bientôt face à notre justice.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T30"
+		},
+	},
+	{
+		id: "R1A1S3T30",
+		question: "Que ces barbares le veuillent ou non.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T31"
+		},
+	},
+	{
+		id: "R1A1S3T31",
+		question:
+			"D’ici là, tu seras celle qui reprendra les rênes de ta sœur. Tu deviendras reine à la place de notre regrettée Thalia.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T32"
+		},
+	},
+	{
+		id: "R1A1S3T32",
+		question: "Vous ne pouvez pas juste me dire ça comme ça !",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T33"
+		},
+	},
+	{
+		id: "R1A1S3T33",
+		question:
+			"Pour moi, Thalia était encore en vie jusqu’il y a quelques minutes !",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T34"
+		},
+	},
+	{
+		id: "R1A1S3T34",
+		question: "Je ne vais pas prendre sa place comme par magie !",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T35"
+		},
+	},
+	{
+		id: "R1A1S3T35",
+		question:
+			"Tu n’as pas le choix, tu es maintenant notre seule héritière.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T36"
+		},
+	},
+	{
+		id: "R1A1S3T36",
+		question: "Mais v-",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T37"
+		},
+	},
+	{
+		id: "R1A1S3T37",
+		question:
+			"Je te laisse aller dans tes quartiers. Nous en avons fini pour aujourd’hui.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T38"
+		},
+	},
+	{
+		id: "R1A1S3T38",
+		question: "J’enverrai quelqu’un te chercher pour le dîner.",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "pnj",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S3T39"
+		},
+	},
+	{
+		id: "R1A1S3T39",
+		question: "...",
+		player_name: "Aelis",
+		player_img: "default",
+		pnj_id: "Tharion",
+		pnj_img: "default",
+		speaking: "player",
+		landscape: "elfique-throne",
+		answered: null,
+		next_card: function () {
+			return "R1A1S4T1"
 		},
 	},
 ]
